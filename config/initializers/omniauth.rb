df90c4b9cd3da5c0f60f7bc2bffb3d3ea5322459
Tommaso Liberati <tommaso.liberati@startegois.com>
2024-04-12 18:27:23 +0200

    added google omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -16,3 +16,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
     scope: 'email', image_size: 'normal'
 end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+    {
+      scope: 'email, profile',
+      prompt: 'select_account',
+      image_aspect_ratio: 'square',
+      image_size: 50
+    }
+end
